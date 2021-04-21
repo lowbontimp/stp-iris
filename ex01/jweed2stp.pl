46846d811591f481a2d4d6c217346d4d6082bc91
lowbontimp <hbim76@gmail.com>
2021-04-21 12:01:41 +0900

    01

--- ex01/jweed2stp.pl
+++ ex01/jweed2stp.pl
@@ -3,19 +3,18 @@
 use strict ;
 use warnings ;
 
-open(my $fh,"cascadia1234.events") ;
-my $n = "0000" ;
+print "dirresp resp01\n" ;
+#print "skip on\n" ;
+open(my $fh,"<cascadia1234.events") ;
+my $n = 0 ;
 while(my $l=<$fh>){
 	chomp($l) ;
 	$n++ ;
-	$n = sprintf("%+05d",$n) ;
-	$n=~s/^\+// ;
+	$n = sprintf("%04d",$n) ;
 	my @c = split(",",$l) ;
 	my ($y,$m,$d,$H,$M,$S) = split("[- :]",$c[1]) ;
-	#print "skip on\n" ;
-	#print "dir data01/$n\n" ;
 	print "dir data01/$n\n" ;
 	print "evt $y/$m/$d,$H:$M:$S $c[2] $c[3] $c[4] $c[8]($c[7]) O(-200) O(1800) 7D % -- BHZ\n" ;
 	print "evt $y/$m/$d,$H:$M:$S $c[2] $c[3] $c[4] $c[8]($c[7]) O(-200) O(1800) 7D % -- HHZ\n" ;
-	}
+}
 close($fh) ;
